2aa9fb173cae671b3156bd6e56451397a14d5fc8
johnsmithsren <rjm1149104294@outlook.com>
2017-02-27 16:42:55 +0800

    version 1.8.3

--- views/map.js
+++ views/map.js
@@ -21,7 +21,7 @@ $('.hello').click(function(){
     //alert('hi');
     var data3=''
     var data1=$.ajax({
-        url: "http://localhost:3000/select",
+        url: "http://www.demacispower.cn:3000/select",
         data: {
             id: 123
         },
@@ -79,9 +79,8 @@ function theLocation(){
 }
 theLocation();
 $('#gps_update').click(function() {
-        alert('hello');
         $.ajax({
-            url: "http://localhost:3000/gps",
+            url: "http://www.demaciaspower.cn:3000/gps",
             data: {
                 latitude:'121.6044990000',
                 longitude:'31.2546250000'
@@ -90,7 +89,7 @@ $('#gps_update').click(function() {
             dataType : "json",
             success:function(data){
                 if(data){
-                    alert('hello world')
+                  //alert('hello world')
                 }
             }
         });
