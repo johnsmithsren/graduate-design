9a4d56a0ef11332f488146de130e8feb5bea5e75
renjm <1149104294@qq.com>
2017-04-18 23:34:47 +0800

    bug

--- controller/qrcode.js
+++ controller/qrcode.js
@@ -39,7 +39,8 @@ qr_code = (function() {
         var _shoe_code=req.query.shoe_code;
         var _starttime=req.query.starttime;
         var _endtime=req.query.endtime;
-        var text='http://www.demaciaspower.cn/getmess?shoe_code='+_shoe_code+'&starttime='+_starttime+'&endtime='+_endtime;
+        var _account=req.query.account;
+        var text='http://www.demaciaspower.cn/getmess?shoe_code='+_shoe_code+'&starttime='+_starttime+'&endtime='+_endtime+'&account='+_account;
         var img = qr.image(text,{size :5});
         res.writeHead(200, {'Content-Type': 'image/png'});
         img.pipe(res);
